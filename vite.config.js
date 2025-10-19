e78d5f505b29df4b4cc0b2c9a3411d20aebcf2d4
DHCEPEDA <dhcepeda@gmail.com>
2025-10-18 22:49:39 -0500

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
+import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
+    extensions: ['.js', '.jsx', '.ts', '.tsx', '.json']
   },
   build: {
     outDir: 'dist-vite',
     rollupOptions: {
       input: {
-        main: path.resolve(__dirname, 'index.html')
+        main: fileURLToPath(new URL('./index.html', import.meta.url))
       }
     }
   },
